ec484c1aca6ea3af627cba06975a665f4013412d
sohamdp <sohamdp28@gmail.com>
2023-03-25 23:03:11 +0530

    Added functionality patient_existIn_hospital,global_patient_id_exist

--- src/main/java/site/sammati/repository/PatientHospitalRepository.java
+++ src/main/java/site/sammati/repository/PatientHospitalRepository.java
@@ -1,9 +1,11 @@
 package site.sammati.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import site.sammati.entity.PatientHospitalMapping;
 
 public interface PatientHospitalRepository extends JpaRepository<PatientHospitalMapping,Integer>
 {
-    //use this hansal
+    @Query("select p.patientId from PatientHospitalMapping p where p.patientId=?1")
+    Integer patientExist(Integer patientId);
 }
